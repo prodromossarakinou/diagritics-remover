1cf7cef6e8da1fd7f28e330822d3ae5f6e93bcee
p.sarakinou@gnomon.com.gr <p.sarakinou@gnomon.com.gr>
2024-10-15 13:02:57 +0300

    added method for case insensitivity;

--- example/example.dart
+++ example/example.dart
@@ -5,4 +5,11 @@ import 'package:diacritics_remover/diacritic.dart';
 void main() {
   //Converts a greek word with tonos
   print(removeDiacritics('Γεια'));
+  print(removeDiacriticsAndIntonation('Γειάα'));
+  print(removeDiacriticsAndIntonation('πρωτεΐνη κοροϊδεύω'));
+
+  if(removeDiacriticsAndIntonation("Πρωτεΐνη") == removeDiacriticsAndIntonation("πρωτεινη")){
+    print('it matches');
+  }
+
 }
--- lib/diacritic.dart
+++ lib/diacritic.dart
@@ -10,3 +10,7 @@ import 'src/replacement_map.dart';
 /// Removes accents and diacritics from the given String.
 String removeDiacritics(String text) =>
     String.fromCharCodes(replaceCodeUnits(text.codeUnits));
+
+/// Case insensitive, intonation insensitive ;
+String removeDiacriticsAndIntonation(String text) =>
+    String.fromCharCodes(replaceCodeUnits(text.toLowerCase().codeUnits));
